8f84dcbea3c578778cf0c42c9e471c044b6ff3c5
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-08-11 22:49:02 +0900

    feat: hardhat test on public testnet goerli
    close #18
    
    - Add node info and account private key info to hardhat.config.js
    - Extend default timeout period of chai
    - Add log out put of tx hash and contract address.

--- contracts/test/sample-test.js
+++ contracts/test/sample-test.js
@@ -1,10 +1,13 @@
 const { expect } = require("chai");
 
 describe("Greeter", function () {
+  this.timeout(60 * 1000);
   it("Should return the new greeting once it's changed", async function () {
     const Greeter = await ethers.getContractFactory("Greeter");
     const greeter = await Greeter.deploy("Hello, world!");
     await greeter.deployed();
+    console.log(`Greeter Deploy Tx: https://goerli.etherscan.io/tx/${greeter.deployTransaction.hash}`);
+    console.log(`Greeter Contract Address: ${greeter.address}`);
 
     expect(await greeter.greet()).to.equal("Hello, world!");
 
@@ -12,6 +15,7 @@ describe("Greeter", function () {
 
     // wait until the transaction is mined
     await setGreetingTx.wait();
+    console.log(`setGreetingTx: https://goerli.etherscan.io/tx/${setGreetingTx.hash}`);
 
     expect(await greeter.greet()).to.equal("Hola, mundo!");
   });
