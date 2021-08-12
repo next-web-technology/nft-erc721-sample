696e41614bd419f9d3f611e445e8f4c433703ad6
YasunoriMATSUOKA <yasunori.matsuoka@gmail.com>
2021-08-12 16:22:28 +0900

    feat: test erc721 contract on goerli network with hardhat
    close #29
    
    - Add additional privateKey's info to hardhat.config.js
    -  Add timeout settings, etherscan link, 2confirmed settings to test code

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -21,7 +21,7 @@ module.exports = {
   networks: {
     goerli: {
       url: `https://goerli.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
-      accounts: [process.env.PRIVATE_KEY]
+      accounts: [process.env.PRIVATE_KEY, process.env.SECOND_PRIVATE_KEY]
     }
   },
   solidity: "0.8.4",
